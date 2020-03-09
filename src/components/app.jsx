d64058388fe34fd9a95724bb32a8d207003230f1
ruijadom <ruijadomingues@gmail.com>
2020-03-09 18:43:14 +0000

    stable

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const App = () => (
   <div className="app">
-    <div className="app_content">
-      <h2>Content</h2>
+    <div className="app-content">
+      <h2 className="app-content-title">React + Webpack</h2>
+      <p className="app-content-text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus tempore reiciendis quas cum, fugiat explicabo
+        nesciunt, quae molestiae blanditiis aliquam rem labore cupiditate animi! Magni eius nam rem reprehenderit fuga?
+      </p>
     </div>
   </div>
 );
